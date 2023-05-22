8853d907f86c3c59d57880ec3edec7578e86c40d
Nick <nicksweetj@gmail.com>
2023-05-22 14:37:31 -0400

    added the weather forcast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var city = document.getElementById("city");
 var userCity = document.getElementById("user-city");
-
+var currentWeather = document.getElementsByClassName("currentWeather");
 
 
 //stores the user city input to local storage
@@ -8,20 +8,42 @@ city.addEventListener("submit", function(e) {
     e.preventDefault();
     var cityName = userCity.value;
     localStorage.setItem("city", cityName);
+    getCoordinates(cityName)
 })
 
 function getCoordinates() {
-    var apiKey = "762fda56a56eacfff2fbc0b55b63619a";
-    var apiCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=${apiKey}";
+    var userChoice = localStorage.getItem("city");
+    var apiKey = '762fda56a56eacfff2fbc0b55b63619a' ;
+    var apiCoordinates = `https://api.openweathermap.org/geo/1.0/direct?q=${userChoice}&appid=${apiKey}`;
     fetch(apiCoordinates)
         .then(function (response) {
             return response.json();
         })
         .then (function (data) {
             console.log(data);
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
+            getWeather(latitude, longitude);
         })
+        .catch(function(error) {
+            console.log('Error:', error);
+        });
+        
+    }
 
+function getWeather(latitude, longitude) {
+    var apiKey = '762fda56a56eacfff2fbc0b55b63619a';
+    var apiWeather = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    fetch(apiWeather)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    //   console.log(data.list[0].main.feels_like); //example of the format to retrive weather variables
+    })
+    .catch(function(error) {
+      console.log('Error:', error);
+    });
 }
-
-
 getCoordinates();
